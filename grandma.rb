163d5eb432723132106dc2714f972d96885a080f
Cristina Kouri <cristina.isabel.kouri@gmail.com>
2018-09-22 14:14:08 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(string)
-  if string == string.upcase
+  if string == "I LOVE YOU GRANDMA!"
     return "NO, NOT SINCE 1938!"
   elsif string == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
